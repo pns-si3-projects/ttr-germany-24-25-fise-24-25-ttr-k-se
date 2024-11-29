d5d7e300816723eade6bfa39ff9568bc1bf688ea
TomBlt <bouillottom@gmail.com>
2024-11-29 17:45:29 +0100

    enum for cities and colors

---
+++ src/main/java/fr/cotedazur/univ/polytech/teamK/board/Colors.java
@@ -0,0 +1,14 @@
+package fr.cotedazur.univ.polytech.teamK.board;
+
+public enum Colors {
+    BLACK,
+    BLUE,
+    RED,
+    WHITE,
+    PINK,
+    YELLOW,
+    GREEN,
+    GRAY,
+    ORANGE,
+    ALL
+}
